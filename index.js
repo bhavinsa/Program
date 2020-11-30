7ecafc328e7391deb40da6ee3de42175d7fe1964
Bhavin <bhavinpatelsa@gmail.com>
2020-11-30 17:17:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function checkIsPrime(input) {
     // check if number is greater than 1
     else if (number > 1) {
         // looping through 2 to number-1
-        for (let i = 2; i < number; i++) {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 isPrime = false;
                 break;
@@ -32,4 +32,4 @@ function checkIsPrime(input) {
     return { msg, isPrime }
 }
 
-console.log(checkIsPrime(4));
+console.log(checkIsPrime(4)); // O(sqrt(n)) 
